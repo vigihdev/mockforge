ffd5214b297c8954a006120e04f9c3926485a818
vigih dev <vigihdev@gmail.com>
2026-01-17 18:05:03 +0700

    💥 2026-01-17 18:05:03

---
+++ tests/DevTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vigihdev\MockForge\Tests;
+
+use PHPUnit\Framework\Attributes\Test;
+
+final class DevTest extends TestCase
+{
+    #[Test]
+    public function ini_hanya_test_awal_untuk_membuat_kerangka_kerja(): void
+    {
+        $this->assertTrue(true);
+    }
+}
